c54ff613ab36610fa3ba5c119319789564b595f3
barkha yadav <143165059+barkhayadav12@users.noreply.github.com>
2024-11-16 09:43:49 +0530

    Update q3_combination_sum_1.cpp

--- Day 9 Recursion/q3_combination_sum_1.cpp
+++ Day 9 Recursion/q3_combination_sum_1.cpp
@@ -31,3 +31,33 @@ public:
         return res;
     }
 };
+
+
+//another way
+class Solution {
+public:
+    void solve(vector<int>&candidates,int target,vector<int>&v,vector<vector<int>>&ans,int ind,int n)
+    {
+        if(target<0)
+        {
+            return;
+        }
+        if(target==0)
+        {
+            ans.push_back(v);
+        }
+        for(int i=ind;i<n;i++)
+        {
+            v.push_back(candidates[i]);
+            solve(candidates,target-candidates[i],v,ans,i,n);
+            v.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>v;
+        int n=candidates.size();
+        solve(candidates,target,v,ans,0,n);
+        return ans;
+    }
+};
